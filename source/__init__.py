e7e4754f19463efa9dbba410ac8b23f9ec0b4111
Vasilis Pitsiavas <vpitsiavas83@gmail.com>
2024-11-15 12:55:43 +0200

    Updated init function

--- source/__init__.py
+++ source/__init__.py
@@ -3,13 +3,16 @@
 # This marks the directory as a package
 # You can also include common imports for ease of access
 
-from .models.arimax_forecast import arimax_forecast
-from .models.xgboost_forecast import xgboost_forecast
+from source.models.arimax_forecast import arimax_forecast
+from source.models.xgboost_forecast import xgboost_forecast
 from source.api import fetch_historical_data, save_to_json
+from source.data_processing import save_to_csv  
+
 
 __all__ = [
     'arimax_forecast', 
     'xgboost_forecast', 
     'fetch_historical_data', 
-    'save_to_json' 
+    'save_to_json',
+    'save_to_csv'
 ]
